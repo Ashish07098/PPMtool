12e4b6ade9b8c5dbefda90e5c1db4db2b49560f1
Ashish <Ashish.kumar07098@gmail.com>
2020-06-14 16:14:12 +0000

    first commit

---
+++ ppmtool/src/test/java/io/jharkhand/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jharkhand.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
